551405d8aa5c25dc3a1b4ed789e36327a9f51f65
osborm10 <osborm10@my.erau.edu>
2021-08-02 00:05:30 -0400

    Found issue when linking resume URL to frontend

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,17 @@
 import "../styles/style.css";
 import "../styles/main.css";
 import Menu from "../components/Menu";
+import App from "next/app"
 
-function MyApp({ Component, pageProps }) {
+const resumeQuery = `*[_type == "resume"] | order(_createdAt desc) [0]{
+  "resumePath": resumeFile.asset->url,
+  resumeTitle
+}`;
+
+function MyApp({ Component, pageProps, resume }) {
   return (
     <>
-      <Menu></Menu>
+      <Menu resumeFile={resume}></Menu>
       <Component {...pageProps} />
       <script async type="text/javascript" src="/js/jquery.min.js"></script>
       <script async type="text/javascript" src="/js/browser.min.js"></script>
@@ -14,6 +20,11 @@ function MyApp({ Component, pageProps }) {
       <script async type="text/javascript" src="/js/main.js"></script>
     </>
   );
+}
+
+export async function getInitialProps(context) {
+  const resume = await sanityClient.fetch(resumeQuery);
+  return { props: { resume } };
 }
 
 export default MyApp;
