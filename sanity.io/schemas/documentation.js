15544eaab90dd285f138ca9ea872bf31d270361e
Miles Osborne <milesosborne@pop-os.localdomain>
2022-08-26 02:57:25 -0400

    Added new resume, updated portable text module for documentation

--- sanity.io/schemas/documentation.js
+++ sanity.io/schemas/documentation.js
@@ -35,9 +35,11 @@ export default {
           type: "block",
           marks: {
             decorators: [
-              { title: "Strong", value: "strong" },
-              { title: "Emphasis", value: "em" },
-              { title: "Code", value: "code" },
+              { "title": "Strong", "value": "strong" },
+              { "title": "Emphasis", "value": "em" },
+              { "title": "Code", "value": "code" },
+              { "title": "Underline", "value": "underline" },
+              { "title": "Strike", "value": "strike-through" },
               { title: "Highlight", value: "highlight" },
             ],
           },
